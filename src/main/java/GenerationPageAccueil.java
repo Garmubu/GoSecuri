1f4f757322ca3b20ee1f116131b0dc8bdd79a8be
kGUILLOT <killianguillot@orange.fr>
2022-02-10 11:15:41 +0100

    ca marche?

--- src/main/java/GenerationPageAccueil.java
+++ src/main/java/GenerationPageAccueil.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
-public class GenerationPägeAccueil {
+public class GenerationPageAccueil {
     public static void main(String[] args){
         HtmlView view = StaticHtml.view(v -> v
                 .html()
