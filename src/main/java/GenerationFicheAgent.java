78bc0531d7aff38e1ada8ae2b17475ffb14995db
kGUILLOT <killianguillot@orange.fr>
2022-02-10 11:38:04 +0100

    recuperation d'infos + html commencé

--- src/main/java/GenerationFicheAgent.java
+++ src/main/java/GenerationFicheAgent.java
@@ -2,9 +2,31 @@ import htmlflow.*;
 import org.xmlet.htmlapifaster.EnumRelType;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class GenerationFicheAgent {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        InputStream ins = null;
+        try {
+            ins = new FileInputStream("data/cberthier.txt");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        Scanner obj = new Scanner(ins);;
+    StringBuilder affectation=new StringBuilder();
+
+       String nom= obj.nextLine();
+       String prenom=obj.nextLine();
+        while (obj.hasNextLine()){
+            if (obj.nextLine()==""){
+                while (obj.hasNextLine()){
+                    affectation.append(obj.nextLine()+";");
+                }
+            }
+        }
+        String affect=affectation.toString();
+        System.out.println(affect);
+
     HtmlView html=StaticHtml.view(v -> v
             .html()
             .head()
@@ -15,6 +37,14 @@ public class GenerationFicheAgent {
             .__()
             .body()
             .div().attrClass("conteneur")
+            .div().attrClass("nom")
+            .p().text(nom)
+            .__()
+            .__()
+
+            .div().attrClass("CNI")
+            .img().attrSrc("C:\\Users\\killi\\IdeaProjects\\GoSecuri\\data\\cberthier.jpg").attrAlt("CNI")
+            .__()
             .__()
             .__()
     );
