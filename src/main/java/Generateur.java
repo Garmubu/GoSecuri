fbb76bfa68e7d51135e8a3b804aba4fe4ea59b76
kGUILLOT <killianguillot@orange.fr>
2022-03-09 15:59:22 +0100

    test

--- src/main/java/Generateur.java
+++ src/main/java/Generateur.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 
 import static j2html.TagCreator.*;
-import static j2html.TagCreator.h1;
+
 
 public class Generateur {
     public static String genererHtml(){
-        ArrayList<String> listeEquipement=new ArrayList<>(2);
+        Equipement e1=new Equipement();
+        Equipement e2=new Equipement();
+        ArrayList<Equipement> listeEquipement=new ArrayList<>(2);
+        listeEquipement.add(e1);
+        listeEquipement.add(e2);
         return html(
                 head(
                         title("Fiche Agent"),
@@ -16,16 +20,28 @@ public class Generateur {
                                 p("nom")
                                 ,div(attrs(".CNI"),
                                         img().withSrc("data/cberthier.jpg").withAlt("CNI"))
-                                ,div(attrs("ListeEquipement")),
+                                ,div(attrs(".ListeEquipement"),
                                 each(listeEquipement, equipement ->
-                                        div(attrs("equipement"),
-                                                p(equipement))
+                                        div(attrs(".equipement"),
+                                                p(String.valueOf(equipement)))
 
-
+                                )
                         )
                 )
                 )
         ).render();
 
+    }
+
+    public static String genererAccueil(){
+        return head(
+                title("Fiche Accueil"),
+                link().withRel("stylesheet").withHref("styles.css")
+        )/*
+        body(
+                main(attrs("content")
+
+        )*/
+                .render();
     }
 }
--- src/main/java/GenerationFicheAgent.java
+++ src/main/java/GenerationFicheAgent.java
@@ -1,14 +1,5 @@
-
-
     import java.io.*;
-    import java.nio.charset.Charset;
-    import java.nio.charset.StandardCharsets;
-    import java.nio.file.Files;
-    import java.nio.file.Path;
-    import java.nio.file.Paths;
     import java.util.Scanner;
-
-    import static j2html.TagCreator.*;
 
     public class GenerationFicheAgent {
     public static void main(String[] args) throws IOException {
