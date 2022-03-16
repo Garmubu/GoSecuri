eb422a0147ad259b7676aa55977357ea91ace2c8
3primspowa4 <ali.guicheri@epsi.fr>
2022-03-16 16:15:23 +0100

    test

---
+++ src/test/java/AgentTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AgentTest {
+
+    @Test
+    public void Get_CA_Serveur() {
+        assertTrue(true);
+
+    }
+}
