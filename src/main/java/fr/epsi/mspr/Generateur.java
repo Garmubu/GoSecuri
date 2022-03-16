54e2515f3c82026b39a4a473bcf98d8388203ffd
Billel <billel.emerton@gmail.com>
2022-03-16 17:25:08 +0100

    waaaaaaaaaaaaa

--- src/main/java/fr/epsi/mspr/Generateur.java
+++ src/main/java/fr/epsi/mspr/Generateur.java
@@ -70,7 +70,7 @@ public class Generateur {
                     )
             ).renderFormatted();
             return render;
-//tgttgtgtggtgtg
+//tgttgtgggtgtg
     }
 
     public static String genererAccueil(ArrayList<Agent> agentList){
