a530b7ce950dfa71fbfbd68c43d0d450ece7af40
Billel <billel.emerton@gmail.com>
2022-03-16 15:30:29 +0100

    wow

--- src/main/java/Generateur.java
+++ src/main/java/Generateur.java
@@ -1,12 +1,125 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static j2html.TagCreator.*;
 
 
 public class Generateur {
+    static class Equipement {
+        private String nom;
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+
+        public ArrayList<Equipement> getListEquipement(String parametre){
+            InputStream inslist = null;
+            try {
+                if (parametre == "equipement"){
+                    inslist = new FileInputStream("data/liste.txt");
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            ArrayList<Equipement> liste=new ArrayList<>();
+
+            Scanner list = new Scanner(inslist);
+            if (parametre == "equipement"){
+
+            }
+            int cpt=0;
+            while (list.hasNextLine()){
+                Equipement equipement = new Equipement();
+                equipement.setNom(list.nextLine());
+                liste.add(equipement);
+                cpt++;
+            }
+            return liste;
+        }
+
+    }
+    static class Agent {
+        private String nom;
+        private String prenom;
+        private String mission;
+        private ArrayList<Equipement> equipement;
+
+        public String getMission() {
+            return mission;
+        }
+
+        public void setMission(String mission) {
+            this.mission = mission;
+        }
+
+        public ArrayList<Equipement> getEquipement() {
+            return equipement;
+        }
+
+        public void setEquipement(ArrayList<Equipement> equipement) {
+            this.equipement = equipement;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+
+        public String getPrenom() {
+            return prenom;
+        }
+
+        public void setPrenom(String prenom) {
+            this.prenom = prenom;
+        }
+
+        public void getInfoAgent(String fichier) {
+            InputStream ins = null;
+            try {
+                ins = new FileInputStream(fichier);
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+            Scanner obj = new Scanner(ins);
+            this.nom=obj.nextLine();
+            this.prenom=obj.nextLine();
+            this.mission=obj.nextLine();
+        /*if (Objects.equals(variable, "nom")) {
+            this.setNom(obj.nextLine());
+        }
+        if (Objects.equals(variable, "prenom")) {
+            obj.nextLine();
+            this.setPrenom(obj.nextLine());
+        }
+        if (Objects.equals(variable, "mission")) {
+            obj.nextLine();
+            obj.nextLine();
+            this.setMission(obj.nextLine());
+        }*/
+        }
+
+        public ArrayList<Agent> getAllAgent() {
+            ArrayList<Agent> agents = new ArrayList<>();
+            InputStream ins = null;
+            try {
+                ins = new FileInputStream("data/staff.txt");
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+            Scanner obj = new Scanner(ins);
+
+
+            return agents;
+        }
+    }
     public static void main(String[] args) throws IOException {
         Agent agent=new Agent();
         agent.getInfoAgent("data/cberthier.txt");
