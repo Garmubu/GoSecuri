2f0ed246aeaeb6b3d4fa0c2347d614141a5b3035
3primspowa4 <ali.guicheri@epsi.fr>
2022-03-17 14:37:37 +0100

    test

--- src/test/java/AgentTest.java
+++ src/test/java/AgentTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import static fr.epsi.mspr.Agent.getAllAgent;
 import static fr.epsi.mspr.Equipement.getListEquipement;
@@ -30,21 +31,25 @@ public class AgentTest {
     public void verificationEquipement() {
         //ÉTANT DONNE un agent et une liste d'equipement
         Agent agent1 = new Agent();
+        boolean test = false;
         ArrayList<Equipement> equipement = new ArrayList<>();
         equipement = getListEquipement("equipement");
         //QUAND Agent est relié à des equipement
-        agent1.getInfoAgent("data/cberthier.txt");
-        agent1.getEquipement("data/cberthier.txt");
+        //ArrayList<Equipement> equipementAgent = agent1.getEquipement("data/cberthier.txt");
         //ALORS les equipements des agents corresponds à la liste d'equipement possible
-        assertTrue(true);
+int cpt=0;
         for ( Equipement equipementAgent : agent1.getEquipement("data/cberthier.txt")){
-            for ( Equipement equipementListe : equipement){
-                if (equipementListe.getNom().contains(equipementAgent.getNom())){
-                    assertTrue(true);
+            for ( Equipement equipementListe : equipement) {
+                cpt++;
+                if (equipementListe.getNom().equals(equipementAgent.getNom())) {
+                    test = true;
+                }
+                if (equipement.size()==cpt && !test) {
+                    fail();
                 }
             }
 
         }
-
+        assertTrue(test);
     }
 }
